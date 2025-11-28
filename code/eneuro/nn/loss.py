397b0776d0f7d9dbd73fc66ea307568b05001c5d
Kailyn1233 <2672504386@qq.com>
2025-11-28 18:36:54 +0800

    Refactor loss functions to use Function base class

--- code/eneuro/nn/loss.py
+++ code/eneuro/nn/loss.py
@@ -1,22 +1,37 @@
-from ..core import Tensor
 import numpy as np
-from abc import ABC,abstractmethod
+from .tensor import Tensor
+from .functions import Function
+from abc import abstractmethod
 
-class Loss(ABC):
-
+class Loss(Function):
+    """损失函数基类"""
+    
+    def __init__(self, reduction='mean'):
+        self.reduction = reduction
+    
     @abstractmethod
-    def __call__(self, pred, target):
+    def forward(self, pred, target):
         pass
+    
+    @abstractmethod
+    def backward(self, gys):
+        pass
+    
+    def __call__(self, pred, target):
+        # 确保输入是Tensor
+        pred = Tensor(pred) if not isinstance(pred, Tensor) else pred
+        target = Tensor(target) if not isinstance(target, Tensor) else target
+        
+       
+        return super().__call__(pred, target)
 
 class MSELoss(Loss):
     """均方误差损失"""
 
-    def __init__(self, reduction='mean'):
-        self.reduction = reduction
-
-    def __call__(self, pred, target):
-        self.pred, self.target = pred, target
-        diff = pred.data - target.data
+    def forward(self, pred, target):
+        self.pred = pred
+        self.target = target
+        diff = pred - target
 
         if self.reduction == 'mean':
             loss = np.mean(diff ** 2)
@@ -25,15 +40,11 @@ class MSELoss(Loss):
         else:
             loss = diff ** 2
 
-        loss_tensor = Tensor(loss, requires_grad=pred.requires_grad)
-        if pred.requires_grad:
-            loss_tensor.set_creator = self  
+        return loss
 
-        return loss_tensor
-
-    def backward(self, grad):
+    def backward(self, gys):
         batch_size = self.pred.shape[0] if len(self.pred.shape) > 0 else 1
-        diff = self.pred.data - self.target.data
+        diff = self.pred - self.target
 
         if self.reduction == 'mean':
             grad_input = 2 * diff / batch_size
@@ -42,90 +53,72 @@ class MSELoss(Loss):
         else:
             grad_input = 2 * diff
 
-
-        grad_input = grad_input * grad
-        
-        if self.pred.requires_grad:
-            # 累加梯度
-            if self.pred.grad is None:
-                self.pred.grad = grad_input
-            else:
-                self.pred.grad += grad_input
+        # 乘以上游梯度
+        grad_input = grad_input * gys
+        return grad_input
 
 class CrossEntropyLoss(Loss):
     """交叉熵损失"""
 
-    def __init__(self, reduction='mean'):
-        self.reduction = reduction
-
-    def __call__(self, pred, target):
-        self.pred, self.target = pred, target
+    def forward(self, pred, target):
+        self.pred = pred
+        self.target = target
 
         # 数值稳定的softmax
-        exp_vals = np.exp(pred.data - np.max(pred.data, axis=1, keepdims=True))
+        exp_vals = np.exp(pred - np.max(pred, axis=1, keepdims=True))
         self.softmax = exp_vals / np.sum(exp_vals, axis=1, keepdims=True)
 
         # 计算交叉熵损失
         if len(target.shape) == 1:
             # 类别索引
             batch_size = pred.shape[0]
-            loss = -np.log(self.softmax[np.arange(batch_size), target.data.astype(int)] + 1e-8)
+            loss = -np.log(self.softmax[np.arange(batch_size), target.astype(int)] + 1e-8)
         else:
             # one-hot编码
-            loss = -np.sum(target.data * np.log(self.softmax + 1e-8), axis=1)
+            loss = -np.sum(target * np.log(self.softmax + 1e-8), axis=1)
 
         if self.reduction == 'mean':
             loss = np.mean(loss)
         elif self.reduction == 'sum':
             loss = np.sum(loss)
 
-        loss_tensor = Tensor(loss, requires_grad=pred.requires_grad)
-        if pred.requires_grad:
-            loss_tensor.set_creator = self  
-        return loss_tensor
+        return loss
 
-    def backward(self, grad):
+    def backward(self, gys):
         batch_size = self.pred.shape[0]
 
         if len(self.target.shape) == 1:
             # 类别索引
             grad_input = self.softmax.copy()
-            grad_input[np.arange(batch_size), self.target.data.astype(int)] -= 1
+            grad_input[np.arange(batch_size), self.target.astype(int)] -= 1
         else:
             # one-hot编码
-            grad_input = self.softmax - self.target.data
+            grad_input = self.softmax - self.target
 
         if self.reduction == 'mean':
             grad_input /= batch_size
 
-        grad_input = grad_input * grad
-        
-        if self.pred.requires_grad:
-            # 累加梯度
-            if self.pred.grad is None:
-                self.pred.grad = grad_input
-            else:
-                self.pred.grad += grad_input
+        # 乘以上游梯度
+        grad_input = grad_input * gys
+        return grad_input
 
 class BCELoss(Loss):
     """二元交叉熵损失"""
     
-    def __init__(self, reduction='mean'):
-        self.reduction = reduction
-    
-    def __call__(self, pred, target):
-        self.pred, self.target = pred, target
+    def forward(self, pred, target):
+        self.pred = pred
+        self.target = target
         
         # 应用sigmoid将预测值映射到(0,1)范围
-        self.pred_sigmoid = 1 / (1 + np.exp(-pred.data))
+        self.pred_sigmoid = 1 / (1 + np.exp(-pred))
         
         # 数值稳定性：防止log(0)
         epsilon = 1e-8
         pred_clipped = np.clip(self.pred_sigmoid, epsilon, 1 - epsilon)
         
         # 计算二元交叉熵
-        bce = - (target.data * np.log(pred_clipped) + 
-                (1 - target.data) * np.log(1 - pred_clipped))
+        bce = - (target * np.log(pred_clipped) + 
+                (1 - target) * np.log(1 - pred_clipped))
         
         if self.reduction == 'mean':
             loss = np.mean(bce)
@@ -134,25 +127,27 @@ class BCELoss(Loss):
         else:
             loss = bce
             
-        loss_tensor = Tensor(loss, requires_grad=pred.requires_grad)
-        if pred.requires_grad:
-            loss_tensor.set_creator = self 
-        return loss_tensor
+        return loss
     
-    def backward(self, grad):
+    def backward(self, gys):
         batch_size = self.pred.shape[0] if len(self.pred.shape) > 0 else 1
         
         # BCE梯度: dL/dpred = (pred_sigmoid - target)
-        grad_input = self.pred_sigmoid - self.target.data
+        grad_input = self.pred_sigmoid - self.target
         
         if self.reduction == 'mean':
             grad_input /= batch_size
         
+        # 乘以上游梯度
+        grad_input = grad_input * gys
+        return grad_input
 
-        grad_input = grad_input * grad
-            
-        if self.pred.requires_grad:
-         if self.pred.grad is None:
-                self.pred.grad = grad_input
-        else:
-                self.pred.grad += grad_input
+# 便捷函数
+def mse_loss(pred, target, reduction='mean'):
+    return MSELoss(reduction)(pred, target)
+
+def cross_entropy_loss(pred, target, reduction='mean'):
+    return CrossEntropyLoss(reduction)(pred, target)
+
+def bce_loss(pred, target, reduction='mean'):
+    return BCELoss(reduction)(pred, target)
