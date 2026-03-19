b06c2fc3ccc67aeac90d85f191525246ccfd8d18
jack <zhongruijie2024@gmail.com>
2026-03-19 21:26:58 +0800

    ai添加深度可分离卷积，转置卷积，空洞卷积

--- code/eneuro/core/__init__.py
+++ code/eneuro/core/__init__.py
@@ -3,6 +3,8 @@ __all__ = []
 from . import functions
 __all__.append('functions')
 from .functions import linear,conv2d,deconv2d,sigmoid,relu,softmax,pooling
+from .functions import depthwise_separable_conv2d,dilated_conv2d,transposed_conv2d
+from .functions import depthwise_conv2d,pointwise_conv2d
 __all__.append('linear')
 __all__.append('conv2d')
 __all__.append('deconv2d')
@@ -10,6 +12,11 @@ __all__.append('sigmoid')
 __all__.append('relu')
 __all__.append('softmax')
 __all__.append('pooling')
+__all__.append('depthwise_separable_conv2d')
+__all__.append('dilated_conv2d')
+__all__.append('transposed_conv2d')
+__all__.append('depthwise_conv2d')
+__all__.append('pointwise_conv2d')
 
 
 from .tensor import Tensor
